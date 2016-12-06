705c6623a5f5fe2a1f6f58066289ffbdf7d4d81a
isedowotomisin <tomisinisedowo@gmail.com>
2016-12-06 14:37:56 +0100

    My Assignment

---
+++ src/name/tomisin.java
@@ -0,0 +1,13 @@
+package name;
+
+/**
+ * Created by Test on 12/6/2016.
+ */
+public class tomisin {
+    public static void main(String[] args) {
+        System.out.print("My Name is \n Isedowo Tomisin ");
+        System.out.print("Am A Student of Uniosun \n From the Dept of Computer Science");
+        System.out.print("\n 300l Precisely ");
+    }
+
+}
